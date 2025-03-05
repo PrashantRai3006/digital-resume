c7c77c9c55b24a0f3de0d1d94ea430ab0075a312
Prashant Rai <prashantrai@Prashants-MacBook-Air.local>
2025-03-05 13:57:01 +0530

    Payment integration

--- src/components/ConditioanlLayout.jsx
+++ src/components/ConditioanlLayout.jsx
@@ -2,7 +2,6 @@ import { useLocation } from "react-router-dom";
 import TemplateSelector from "./TemplateSelecter";
  const ConditionalLayout = ({ children}) => {
     const location = useLocation();
-    console.log(location.pathname);
   return !location.pathname.startsWith('/digital-resume') ? children : <><TemplateSelector/>{children}</>;
 };
 export default ConditionalLayout;
